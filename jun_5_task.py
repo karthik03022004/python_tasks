8015b68699eee5b296b03fd4e885159d35b6b126
karthik03022004 <karthiksunny222@gmail.com>
2025-07-06 18:32:26 +0530

    added lcm examples

--- jun_5_task.py
+++ jun_5_task.py
@@ -1,4 +1,4 @@
-'''                    LCM of 2 numbers             '''
+'''                    LCM of 2 numbers             
 n1=int(input())
 n2=int(input())
 big=0
@@ -16,5 +16,45 @@ while lcm_found==False:
         lcm_found=True
     else:
         lcm+=big
+
+'''
+
+'''LCM (least common multiple): finding  common multiple least from 2 numbers'''
+
+# (3,9)lcm=9
+# 9=9,18,27...
+# 3=3,6,9,12...
+
+# (5,10)lcm=10
+# 5=5,10,15,20,25..
+# 10=10,20,30...
+
+# (8,12)lcm=24
+# 12=12,24,36,48,60...
+# 8=8,16,24,32,40....
+
+# (11,13)lcm=143
+# 11=11,22,33,44,55,66,77,8,99,110,121,132,143..,
+# 13=13,26,39 52,65,78,91,104,117,130,143..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
