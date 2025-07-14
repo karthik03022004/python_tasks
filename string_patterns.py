8ea25b25b139b03cab253570a56c88942065dae4
karthik03022004 <karthiksunny222@gmail.com>
2025-07-14 22:12:13 +0530

    y,Y,M shapes pushed

--- string_patterns.py
+++ string_patterns.py
@@ -59,4 +59,58 @@ for i in range(1,rows+1):
     print(pattern)
 
 
+# jun14 task 
+rows=9 #Y shape
+mid=(rows//2)+1
+for i in range(1,rows+1):
+    res=""
+    for j in range(1,rows+1):
+        if(i<=mid):
+            if(i==j or i+j==rows+1):
+                res+="*"+" "
+            else:
+                res+=" "+" "
+        else:
+            if(j==mid):
+                res+="*"+" "
+            else:
+                res+=" "+" "
+    print(res)
+print()         
+rows=7 #y shape
+mid=(rows//2)+1
+for i in range(1,rows+1):
+    res=""
+    for j in range(1,rows+1):
+        if(i<=mid):
+            if(i==j or i+j==rows+1):
+                res+="*"+" "
+            else:
+                res+=" "+" "
+        else:
+            if(i+j==rows+1):
+                res+="*"+" "
+            else:
+                res+=" "+" "
+    print(res)
+    
+print()
+rows=5 #M shape
+mid=(rows//2)+1
+for i in range(1,rows+1):
+    res=""
+    for j in range(1,rows+1):
+        if(i<=mid):
+            if(j==1 or j==rows or i==j or i+j==rows+1):
+                res+="*"+" "
+            else:
+                res+=" "+" "
+        else:
+            if(j==1 or j==rows):
+                res+="*"+" "
+            else:
+                res+=" "+" "
+    print(res)
+
+
     
