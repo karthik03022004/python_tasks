b7dcccb2403f85c79646121cd4e4a06a49ab4d42
karthik03022004 <karthiksunny222@gmail.com>
2025-07-18 20:49:02 +0530

    matrix

--- list/list.py
+++ list/list.py
@@ -10,7 +10,7 @@
 # count()
 # index()
 # copy()
-
+'''----------------------------------------------------------------'''
 # # ->sum of elememts-sum() method
 # num=[1,2,3,4,5,6,7,8,-9]
 # print("sum:",sum(num))
@@ -61,39 +61,184 @@
 # print(tot)
 
 # index logic
-food=["birayani","noodels","fry","icecream","mommos","fry","fry"]
-item="fry"
-for i in range(len(food)):
-    if(food[i]==item):
-        print(f"{food[i]} is at {i+1} position")
-        break
-# count logic
-count=0
-for i in food:
-    if(i==item):
-        count+=1
-print(count)
-# reverse logic
-rev=[]
-for i in range(len(food)-1,-1,-1):
-    rev.append(food[i])
-print(rev)
+# food=["birayani","noodels","fry","icecream","mommos","fry","fry"]
+# item="fry"
+# for i in range(len(food)):
+#     if(food[i]==item):
+#         print(f"{food[i]} is at {i+1} position")
+#         break
 
-#task-  list=[1,"A",2,"B","a","z"]  output:us=AB ls=az sum=3
-l=[1,"A",2,"B","a",'z']
-us=""
-ls=""
-sum=0
-for i in l:
-    i=str(i)
-    if(ord(i)>=65 and ord(i)<=90):
-        us+=i
-    elif(ord(i)>=97 and ord(i)<=122):
-        ls+=i
-    else:
+# # count logic
+# count=0
+# for i in food:
+#     if(i==item):
+#         count+=1
+# print(count)
+
+# # reverse logic
+# rev=[]
+# for i in range(len(food)-1,-1,-1):
+#     rev.append(food[i])
+# print(rev)
+
+# #task-  list=[1,"A",2,"B","a","z"]  output:us=AB ls=az sum=3
+# l=[1,"A",2,"B","a",'z']
+# us=""
+# ls=""
+# sum=0
+# for i in l:
+#     i=str(i)
+#     if(ord(i)>=65 and ord(i)<=90):
+#         us+=i
+#     elif(ord(i)>=97 and ord(i)<=122):
+#         ls+=i
+#     else:
         
-        sum+=int(i)
-print(sum,us,ls)
+#         sum+=int(i)
+# print(sum,us,ls)
+# '''---------------------------------------------------------------------------'''
+# nested list
+# sum of numbers in a nested list 
+nested=[1,[2,3]]
+sum=0
+for i in nested:
+    if(isinstance(i,list)):
+        for j in i:
+            sum+=j
+    else:
+        sum+=i
+print("sum of nested list:",sum)
+
+# print(isinstance(1,int))
+# print(isinstance(2.3,float))
+# print(isinstance(True,bool))
+# print(isinstance({},dict))
+# ---task - to check the data type of an user input 
+# n=eval(input())
+# if(isinstance(n,int)):
+#     print("class <int>")
+# elif(isinstance(n,float)):
+#     print("class <float>")
+# elif(isinstance(n,bool)):
+#     print("class <bool>")
+# elif(isinstance(n,complex)):
+#     print("class <complex>")
+# elif(isinstance(n,str)):
+#     print("class <str>")
+# elif(isinstance(n,list)):
+#     print("class <list>")
+# elif(isinstance(n,tuple)):
+#     print("class <tuple>")
+# elif(isinstance(n,dict)):
+#     print("class <dict>")
+# else:
+#     print("class <set>")
+
+num=[[-1,-2],[-3,-4],[-5,6]] # find the max sum value of a sublist
+max_list=[]
+# max_val=0 # won't work for neg val in sublist
+max_val=num[0][0]
+for i in num:
+    sum=0
+    for j in i:
+        sum+=j
+    if(sum>max_val):
+        max_val=sum
+        max_list=i
+print(max_val,max_list)
+
+num=[[1,5],[3,-6],[100,900]]
+maxi=num[0][0]
+for i in num:
+    for j in i:
+        if(j>maxi):
+            maxi=j
+print("maximum value:",maxi)
+# task to find mini val in a sub list
+mini=num[0][0]
+for i in num:
+    for j in i:
+        if(j<mini):
+            mini=j
+print("minimum val in sub list:",mini)
+
+# matrix - finding matrix is square or not
+matrix=[
+    [1,2,3,3],
+    [4,5,6,3],
+    [8,9,10,3],
+    [7,7,7,7]
+]
+rows=len(matrix)
+sq_matrix=True
+for i in matrix:
+    if(rows!=len(i)):
+        sq_matrix=False
+    
+print("square matrix") if sq_matrix else print("not a square matrix")
+'''-----------------------------------------------------------------------'''
+# checking 2 matrices has has same rows and cols
+mat1=[
+    [1,2],
+    [3,4]
+]
+mat2=[
+    [5,6],
+    [7,8]
+]
+if(len(mat1)!=len(mat2)):
+    print("rows and cols are not equal so arthmetic op are not possible.")
+else:
+    possible=True
+    for i in range(len(mat1)):
+        if(len(mat1[i])!=len(mat2[i])):
+            possible=False
+    print("matrices have same rows and cols") if possible else print("rows and cols are not equal so arthmetic op are not possible.")
+# addition of 2 matrices
+addition=[]
+for i in range(len(mat1)):
+    row=[]
+    for j in range(len(mat1)):
+        sum=mat1[i][j]+mat2[i][j]
+        row.append(sum)
+    addition.append(row)
+print(addition)
+# subraction of 2 matrices
+subraction=[]
+for i in range(len(mat1)):
+    row=[]
+    for j in range(len(mat1)):
+        res=mat1[i][j]-mat2[i][j]
+        row.append(res)
+    subraction.append(row)
+print(subraction)
+# multipliation of 2 matrices
+# check condition->cols of mat1==rows of mat2
+mul_check=True
+for i in mat1:
+    if(len(i)!=len(mat2)):
+        mul_check=False
+        break
+if(mul_check):
+    mul=[]
+    for i in range(len(mat1)):
+        row=[]
+        for j in range(len(mat2[0])):
+            res=0
+            for k in range(len(mat2)):
+                res+=mat1[i][k]*mat2[k][j]
+            row.append(res)
+        print(row)
+    
+            
+
+
+
+
+
+
+
+
 
 
 
