786ab1da87464700e5974e2d598158f2b0297577
karthik03022004 <karthiksunny222@gmail.com>
2025-07-23 20:06:28 +0530

    s shape

--- dsa/ps.py
+++ dsa/ps.py
@@ -1,16 +1,25 @@
-n = 3
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if i ==1 or j ==1 or j == 2 or i == n:
-            print("* ",end='')
+'''
+* * * * *
+*   *
+* * * * *
+    *   *
+* * * * *        
+'''
+rows=5
+mid=(rows//2)+1
+for i in range(1,rows+1):
+    pat=""
+    for j in range(1,rows+1):
+        if(i<=mid):
+            if(i==1 or j==1 or i==mid or j==mid):
+                pat+="*"+" "
+            else:
+                pat+=" "+" "
+
         else:
-            print(' ',end='')
-    print()
-n = 2
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if   j == 2 or i == n:
-            print("* ",end='')
-        else:
-            print(' ',end='')
-    print()
+            
+            if(j==rows or i==rows or j==mid):
+                pat+="*"+" "
+            else:
+                pat+=" "+" "
+    print(pat)
